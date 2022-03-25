b01cc9867c5bf6fdb76c1046eae40cb437058934
afmartinez <afmartinez@grupovision.org>
2022-03-25 09:06:16 -0600

    curso Python 1 - 10

---
+++ video10.py
@@ -0,0 +1,14 @@
+#-----Curso python video 10 condicionales-----
+print("Programa de evaluación de notas de alumnos")
+
+nota_alumno=input("Introduce la nota del alumno: ") #el usuario ingresa la variable a evaluar
+
+def evaluacion(nota):                   #función
+    valoracion="aprobado"
+    if nota<5:                          #condicion
+        valoracion="reprobado"          #si se cumple la condicion
+
+    return valoracion
+
+print(evaluacion(int(nota_alumno)))     #transforma la variable a entero, se la pasa a la función y se imprime el resultado
+#---------------------------------------------
